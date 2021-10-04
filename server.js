336e72cacad89ba747102f1f157c8d483500deda
Lawe Zangena <lawza02@gmail.com>
2021-10-04 12:05:34 +0200

    structural change

--- server.js
+++ server.js
@@ -1,37 +1,24 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const fs = require ('fs')
+const express = require('express');
+const fs = require('fs');
 
-const app = express()
-const PORT = process.env.PORT || 8042;
+const app = express();
+const port = 8037;
 
-app.get('/', (req,res)=> {
-    fs.readFile('index.html', (err, data) =>{
-        res.setHeader('content-Type', 'text/html')
-        res.setHeader('content-Length', data.length);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+    fs.readFile('public/index.html', (err, data) => {
+        console.log(err);
+        res.setHeader('Content-Type', 'text/html');
         res.send(data);
-    })
-})
+    });
+    
 
-app.get('/stilar.css', (req, res) =>{
-    fs.readFile('stilar.css', (err, data)=>{
-        res.setHeader('content-type', 'text/css')
-        res.setHeader('contetnt-Length', data.length)
-        res.send(data);
-    })
+});
 
-})
-app.get('/anka', (req, res) =>{
-    fs.readFile('lawe_1.jpg', (err, data)=>{
-        res.setHeader('content-type', 'image/jpg')
-        res.setHeader('contetnt-Length', data.length)
-        res.send(data);
-    })
+app.listen(port);
 
-})
-
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-  })
-
+console.log('Server up and running, listning on port: ' + port);
