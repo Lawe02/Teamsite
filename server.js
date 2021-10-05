9e634d5ea16110e2c970948e4ef895a1631d928b
Lawe Zangena <lawza02@gmail.com>
2021-10-05 16:14:59 +0200

    ändrade js

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-'use strict';
+'use strict'
 
 const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const PORT = 8037;
+const PORT = process.env.PORT || 8034;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-
     fs.readFile('public/index.html', (err, data) => {
-        console.log(err);
         res.setHeader('Content-Type', 'text/html');
+       res.setHeader('Content-lengt', data.length)
         res.send(data);
-    });
-    
 
-});
+    })
+})
 
-app.listen(PORT);
 
-console.log('Server up and running, listning on port: ' + PORT);
+
+
+app.listen(PORT, () => {
+    console.log(`the app listen to ${PORT}`);
+})
